213d282096980cefc969659c575957b2462ddaea
Matheuspz <mpzenere@gmail.com>
2023-05-15 12:28:19 -0300

    caixas

--- Mystery-Food/caixas/script.js
+++ Mystery-Food/caixas/script.js
@@ -1,19 +1,27 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function Dropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
   // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
+
+function AbrirCarrinho() {
+  document.getElementById("mySidenav").style.width = "315px";
+}
+
+function FecharCarrinho() {
+  document.getElementById("mySidenav").style.width = "0px";
+}
